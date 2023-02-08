a774298395cf15a724e3783609285e842652edc3
swastik <swastkk@gmail.com>
2023-02-08 18:37:13 +0530

    Theme added
    Signed-off-by: swastik <swastkk@gmail.com>

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -1,28 +1,36 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Project information
 
-project = 'gitpedia'
+project = 'GITpedia'
 copyright = '2023, Swastik Sharma'
 author = 'Swastik Sharma'
-release = '0.0.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+release = '0.1'
+version = '0.1.0'
 
-extensions = []
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = []
 
-language = 'English'
+# -- Options for HTML output
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'sphinx_rtd_theme'
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
--- documentation/source/index.rst
+++ documentation/source/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to gitpedia's documentation!
+Welcome to Gitpedia's documentation!
 ====================================
 
 .. toctree::
